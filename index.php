ba9bc6e008aece163b8e8a9b29ff634f3677c9fa
El Khawarzmi | Coding <61169493+elkhawarizmicoding@users.noreply.github.com>
2020-02-18 02:21:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
     /*
-        Developper By Anodaz App
-        name: Achwak App
-        Description: This application is a site that includes all the information and news of the world and contains a platform to watch high-quality movies and application store platform
-        Method: MVC with PHP and MYSQL and AJAX ...
-        App: achwak.anodazapp.com
-        Language: arabic (ar)
+        Developper: By El Khawarizmi | Coding
+        name: SGRE APP
+        Description: SGRE APP
+        Design pattern: MVC
+        Langage programmation: PHP
+        Database: MySql
+        App: achwak.adnew.net
+        Language: english (en)
         Version: 0.0.1
     */
     
